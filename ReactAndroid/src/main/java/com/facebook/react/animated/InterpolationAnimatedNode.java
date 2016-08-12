7d89328f04662ff21077340346cd4ad7da0b120f
Janic Duplessis <janicduplessis@gmail.com>
2016-08-11 21:48:52 -0400

    [NativeAnimated][Android] Add support for extrapolation

--- ReactAndroid/src/main/java/com/facebook/react/animated/InterpolationAnimatedNode.java
+++ ReactAndroid/src/main/java/com/facebook/react/animated/InterpolationAnimatedNode.java
@@ -12,6 +12,32 @@ import javax.annotation.Nullable;
  */
 /*package*/ class InterpolationAnimatedNode extends ValueAnimatedNode {
 
+  private enum ExtrapolateType {
+    IDENTITY("identity"),
+    CLAMP("clamp"),
+    EXTEND("extend");
+
+    private final String mName;
+
+    ExtrapolateType(String name) {
+      mName = name;
+    }
+
+    public static ExtrapolateType fromString(String name) {
+      for (ExtrapolateType type : ExtrapolateType.values()) {
+        if (type.toString().equalsIgnoreCase(name)) {
+          return type;
+        }
+      }
+      throw new IllegalArgumentException("Unsupported extrapolate type : " + name);
+    }
+
+    @Override
+    public String toString() {
+      return mName;
+    }
+  }
+
   private static double[] fromDoubleArray(ReadableArray ary) {
     double[] res = new double[ary.size()];
     for (int i = 0; i < res.length; i++) {
@@ -25,19 +51,56 @@ import javax.annotation.Nullable;
       double inputMin,
       double inputMax,
       double outputMin,
-      double outputMax) {
+      double outputMax,
+      ExtrapolateType extrapolateLeft,
+      ExtrapolateType extrapolateRight) {
+    double result = value;
+
+    // Extrapolate
+    if (result < inputMin) {
+      switch (extrapolateLeft) {
+        case IDENTITY:
+          return result;
+        case CLAMP:
+          result = inputMin;
+          break;
+        case EXTEND:
+          break;
+      }
+    }
+
+    if (result > inputMax) {
+      switch (extrapolateRight) {
+        case IDENTITY:
+          return result;
+        case CLAMP:
+          result = inputMax;
+          break;
+        case EXTEND:
+          break;
+      }
+    }
+
     return outputMin + (outputMax - outputMin) *
-      (value - inputMin) / (inputMax - inputMin);
+      (result - inputMin) / (inputMax - inputMin);
   }
 
-  /*package*/ static double interpolate(double value, double[] inputRange, double[] outputRange) {
+  /*package*/ static double interpolate(
+      double value,
+      double[] inputRange,
+      double[] outputRange,
+      ExtrapolateType extrapolateLeft,
+      ExtrapolateType extrapolateRight
+  ) {
     int rangeIndex = findRangeIndex(value, inputRange);
     return interpolate(
       value,
       inputRange[rangeIndex],
       inputRange[rangeIndex + 1],
       outputRange[rangeIndex],
-      outputRange[rangeIndex + 1]);
+      outputRange[rangeIndex + 1],
+      extrapolateLeft,
+      extrapolateRight);
   }
 
   private static int findRangeIndex(double value, double[] ranges) {
@@ -52,11 +115,15 @@ import javax.annotation.Nullable;
 
   private final double mInputRange[];
   private final double mOutputRange[];
+  private final ExtrapolateType mExtrapolateLeft;
+  private final ExtrapolateType mExtrapolateRight;
   private @Nullable ValueAnimatedNode mParent;
 
   public InterpolationAnimatedNode(ReadableMap config) {
     mInputRange = fromDoubleArray(config.getArray("inputRange"));
     mOutputRange = fromDoubleArray(config.getArray("outputRange"));
+    mExtrapolateLeft = ExtrapolateType.fromString(config.getString("extrapolateLeft"));
+    mExtrapolateRight = ExtrapolateType.fromString(config.getString("extrapolateRight"));
   }
 
   @Override
@@ -84,6 +151,6 @@ import javax.annotation.Nullable;
       throw new IllegalStateException("Trying to update interpolation node that has not been " +
         "attached to the parent");
     }
-    mValue = interpolate(mParent.mValue, mInputRange, mOutputRange);
+    mValue = interpolate(mParent.mValue, mInputRange, mOutputRange, mExtrapolateLeft, mExtrapolateRight);
   }
 }
