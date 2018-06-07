9b940ac55b4fd49ce2db185fc63177246390e1be
Janic Duplessis <janicduplessis@gmail.com>
2018-06-07 11:31:03 -0400

    Add applyDecoratedDescriptor babel helper

--- Libraries/polyfills/babelHelpers.js
+++ Libraries/polyfills/babelHelpers.js
@@ -604,3 +604,40 @@ function _iterableToArray(iter) {
 }
 
 babelHelpers.iterableToArray = _iterableToArray;
+
+function _applyDecoratedDescriptor(
+  target,
+  property,
+  decorators,
+  descriptor,
+  context,
+) {
+  var desc = {};
+  Object['ke' + 'ys'](descriptor).forEach(function(key) {
+    desc[key] = descriptor[key];
+  });
+  desc.enumerable = !!desc.enumerable;
+  desc.configurable = !!desc.configurable;
+  if ('value' in desc || desc.initializer) {
+    desc.writable = true;
+  }
+  desc = decorators
+    .slice()
+    .reverse()
+    .reduce(function(desc, decorator) {
+      return decorator(target, property, desc) || desc;
+    }, desc);
+  if (context && desc.initializer !== void 0) {
+    desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
+    desc.initializer = undefined;
+  }
+  if (desc.initializer === void 0) {
+    // This is a hack to avoid this being processed by 'transform-runtime'.
+    // See issue #9.
+    Object['define' + 'Property'](target, property, desc);
+    desc = null;
+  }
+  return desc;
+}
+
+babelHelpers.applyDecoratedDescriptor = _applyDecoratedDescriptor;
